006b791977d5f7c5000e56788386b603cceed1e8
André Santos <andre@dexdev.com.br>
2019-02-27 20:29:00 -0300

    Exploring JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,54 @@
 'use strict';
 
-console.log('App.js is running!');
-
 // JSX - Javascript XML
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'Indecision App'
+  React.createElement(
+    'h1',
+    null,
+    'Andre Santos'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is some info'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
 );
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Andre Santos'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age:36'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: Aracaju'
+  )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
-console.log('App.js is running!');
-
 // JSX - Javascript XML
-var template = <h1>Indecision App</h1>;
+var template = (
+  <div>
+    <h1>Andre Santos</h1>  
+    <p>This is some info</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+)
+var templateTwo = (
+    <div>
+      <h1>Andre Santos</h1>
+      <p>Age:36</p>
+      <p>Location: Aracaju</p>
+    </div>
+  )
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);

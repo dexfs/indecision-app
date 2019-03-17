d24671a341a3a4543fc51ace3aca5e8d71e18f34
André Santos <andre@dexdev.com.br>
2019-03-17 14:15:05 -0300

    Added components

---
+++ src/components/IndecisionApp.js
@@ -0,0 +1,93 @@
+import React from "react"
+import AddOption from "./AddOption"
+import Header from "./Header"
+import Action from "./Action"
+import Options from "./Options"
+import OptionModal from "./OptionModal"
+
+export default class IndecisionApp extends React.Component {
+  state = {
+    options: [],
+    selectedOption: undefined
+  }
+
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem("options")
+      const options = JSON.parse(json)
+      if (options) {
+        this.setState(() => ({ options }))
+      }
+    } catch (error) {}
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options)
+      localStorage.setItem("options", json)
+    }
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount")
+  }
+  handleClearSelectedOption = () => {
+    this.setState(() => ({
+      selectedOption: undefined
+    }))
+  }
+
+  handleDeleteOptions = () => {
+    this.setState(() => ({
+      options: []
+    }))
+  }
+  handleDeleteOption = (optionToRemove) => {
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) => {
+        return optionToRemove !== option
+      })
+    }))
+  }
+  handlePick = () => {
+    const randomNum = Math.floor(Math.random() * this.state.options.length)
+    const option = this.state.options[randomNum]
+    this.setState({
+      selectedOption: option
+    })
+  }
+  handleAddOption = (option) => {
+    if (!option) {
+      return "Enter valid value to add item"
+    } else if (this.state.options.indexOf(option) > -1) {
+      return "This options already exists"
+    }
+    this.setState((prevState) => ({ options: prevState.options.concat(option) }))
+  }
+  render() {
+    const title = "Indecision"
+    const subtitle = "Put your life in he hands of a computer"
+    return (
+      <div>
+        <Header title={title} subtitle={subtitle} />
+        <div className='container'>
+          <Action hasOptions={this.state.options.length > 0} handlePick={this.handlePick} />
+          <div className='widget'>
+            <Options
+              options={this.state.options}
+              handleDeleteOptions={this.handleDeleteOptions}
+              handleDeleteOption={this.handleDeleteOption}
+            />
+            <AddOption handleAddOption={this.handleAddOption} />
+          </div>
+          <OptionModal
+            selectedOption={this.state.selectedOption}
+            handleClearSelectedOption={this.handleClearSelectedOption}
+          />
+        </div>
+      </div>
+    )
+  }
+}
